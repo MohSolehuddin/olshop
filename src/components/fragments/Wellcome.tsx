369aaf7486b406e2d29212fdc558aaeb7fb14fff
MohSolehuddin <moh.solehuddin190805@gmail.com>
2024-06-03 19:39:34 +0700

    fix: responsif

--- src/components/fragments/Wellcome.tsx
+++ src/components/fragments/Wellcome.tsx
@@ -7,17 +7,19 @@ const Wellcome = ({
   children: React.ReactNode;
 }) => {
   return (
-    <section className="wellcome bg-zinc-800 min-h-screen flex place-items-center gap-4 justify-center text-center flex-nowrap">
-      <section className="layanan flex flex-col place-items-center">
+    <section className="wellcome bg-zinc-800 min-h-screen flex place-items-center gap-0 md:gap-4 justify-center text-center flex-wrap">
+      <section className="layanan flex flex-col place-items-center w-1/2">
         <Image
           src="/logo.png"
           alt="Logo toko"
-          className="rounded-full mb-5"
+          className="rounded-full mb-5 mt-16 sm:mt-16 lg:mt-0"
           width="160"
           height="160"
         />
-        <h1 className="text-[#00feff] text-5xl">{children}</h1>
-        <p className="text-sky-50 w-2/3">
+        <h1 className="text-[#00feff] lg:text-5xl md:text-4xl sm:text-2xl text-xl">
+          {children}
+        </h1>
+        <p className="text-sky-50 w-full md:w-4/5 lg:w-2/3 gap-1">
           Jasa pembuatan website sat set abisss, ga perlu mahal mulai 300rb aja
           udah bisa punya website
         </p>
